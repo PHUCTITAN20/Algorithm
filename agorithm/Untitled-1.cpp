89e4dea4c1e3d0a5ebf4607249bc416ef479af01
Nguyen_Huu_Phuc <142430293+PHUCTITAN20@users.noreply.github.com>
2025-07-30 12:03:41 +0700

    Fix array length calculation

--- agorithm/Untitled-1.cpp
+++ agorithm/Untitled-1.cpp
@@ -21,7 +21,7 @@ void Sap_xep_chon( int a[], int n){
 
 int main(){
     int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr);
+    int n = sizeof(arr) / sizeof(arr[0]);
     // in mang chua duoc sap xep
     printf("Original array: ");
     for (int i=0; i<= n-1; i++){
